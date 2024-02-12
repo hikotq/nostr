fdb17680dae4f2352dd545aeffd9dc5c05b3e893
hikotq <hikotq@gmail.com>
2024-02-12 15:16:00 +0900

    REQメッセージを送信して投稿を取得

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-use std::env;
+mod req;
 
-use futures_util::{future, pin_mut, StreamExt};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use crate::req::{Filter, Req};
+use futures_util::{pin_mut, SinkExt, StreamExt};
+use std::env;
+use tokio::io::AsyncWriteExt;
+use tokio_tungstenite::connect_async;
 
 #[tokio::main]
 async fn main() {
-    let connect_addr =
-        env::args().nth(1).unwrap_or_else(|| panic!("this program requires at least one argument"));
+    let connect_addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| panic!("this program requires at least one argument"));
 
     let url = url::Url::parse(&connect_addr).unwrap();
-
-    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    tokio::spawn(read_stdin(stdin_tx));
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
 
-    let (write, read) = ws_stream.split();
+    let (mut write, read) = ws_stream.split();
 
-    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
     let ws_to_stdout = {
         read.for_each(|message| async {
             let data = message.unwrap().into_data();
@@ -27,21 +26,11 @@ async fn main() {
         })
     };
 
-    pin_mut!(stdin_to_ws, ws_to_stdout);
-    future::select(stdin_to_ws, ws_to_stdout).await;
-}
+    let req = Req {
+        id: "testtesttesttesttest".to_string(),
+        filter: Filter::new().kinds(vec![1]),
+    };
+    write.send(req.serialize().into()).await.unwrap();
 
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-    loop {
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n,
-        };
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
-    }
+    ws_to_stdout.await;
 }
---
+++ src/req.rs
@@ -0,0 +1,103 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+pub struct Req {
+    pub id: String,
+    pub filter: Filter,
+}
+
+impl Req {
+    pub fn serialize(&self) -> String {
+        format!(
+            r#"["REQ","{}",{}]"#,
+            self.id,
+            serde_json::to_string(&self.filter).unwrap()
+        )
+        .to_string()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Filter {
+    // イベントのID、もしくは先頭部分（プレフィクス）のリスト
+    #[serde(skip_serializing_if = "Option::is_none")]
+    ids: Option<Vec<String>>,
+    // 公開鍵、もしくは先頭部分のリスト
+    #[serde(skip_serializing_if = "Option::is_none")]
+    authors: Option<Vec<String>>,
+    // イベントの種類の数字のリスト
+    #[serde(skip_serializing_if = "Option::is_none")]
+    kinds: Option<Vec<u32>>,
+    // "e"タグで参照されたイベントIDのリスト
+    #[serde(rename = "#e", skip_serializing_if = "Option::is_none")]
+    e_tags: Option<Vec<String>>,
+    // "p"タグで参照された公開鍵のリスト
+    #[serde(rename = "#p", skip_serializing_if = "Option::is_none")]
+    p_tags: Option<Vec<String>>,
+    // UNIXタイムスタンプ（秒単位の整数値）。パスするには、イベントはこれより新しくなければならない
+    #[serde(skip_serializing_if = "Option::is_none")]
+    since: Option<i64>,
+    // UNIXタイムスタンプ（秒単位の整数値）。パスするには、イベントはこれより古くなければならない
+    #[serde(skip_serializing_if = "Option::is_none")]
+    until: Option<i64>,
+    // 初回の問い合わせで返されるイベントの個数の上限
+    #[serde(skip_serializing_if = "Option::is_none")]
+    limit: Option<usize>,
+}
+
+impl Filter {
+    // 新しいクエリインスタンスを生成するためのコンストラクタ
+    pub fn new() -> Self {
+        Self {
+            ids: None,
+            authors: None,
+            kinds: None,
+            e_tags: None,
+            p_tags: None,
+            since: None,
+            until: None,
+            limit: None,
+        }
+    }
+
+    // 各フィールドを設定するメソッド。selfの所有権を取り、更新された新しいインスタンスを返す。
+    pub fn ids(mut self, ids: Vec<String>) -> Self {
+        self.ids = Some(ids);
+        self
+    }
+
+    pub fn authors(mut self, authors: Vec<String>) -> Self {
+        self.authors = Some(authors);
+        self
+    }
+
+    pub fn kinds(mut self, kinds: Vec<u32>) -> Self {
+        self.kinds = Some(kinds);
+        self
+    }
+
+    pub fn e_tags(mut self, e_tags: Vec<String>) -> Self {
+        self.e_tags = Some(e_tags);
+        self
+    }
+
+    pub fn p_tags(mut self, p_tags: Vec<String>) -> Self {
+        self.p_tags = Some(p_tags);
+        self
+    }
+
+    pub fn since(mut self, since: i64) -> Self {
+        self.since = Some(since);
+        self
+    }
+
+    pub fn until(mut self, until: i64) -> Self {
+        self.until = Some(until);
+        self
+    }
+
+    pub fn limit(mut self, limit: usize) -> Self {
+        self.limit = Some(limit);
+        self
+    }
+}
